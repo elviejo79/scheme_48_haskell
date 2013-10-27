530677c66508aa1673644e74e84f70cf7ac5fe21
elviejo79 <agarciafdz@gmail.com>
2013-10-26 23:14:21 -0500

    some pretty basic evaluation

---
+++ 42_beginning_evaluator_primitives.hs
@@ -0,0 +1,91 @@
+module Main(main) where
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser()
+spaces = skipMany1 space
+
+-- An atom is a letter or symbol, followed by any number of letters, digits, or symbols
+parseAtom :: Parser LispVal
+parseAtom = do first <- letter <|> symbol
+               rest <- many (letter <|> digit <|> symbol)
+               let atom = [first] ++ rest
+               return $ case atom of
+                             "#t" -> Bool True
+                             "#f" -> Bool False
+                             otherwise -> Atom atom
+
+parseString :: Parser LispVal
+parseString = do char '"'
+                 x <- many (noneOf "\"")
+                 char '"'
+                 return $ String x
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+            char '\''
+            x <- parseExpr
+            return $ List [Atom "quote", x]
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+                head <- endBy parseExpr spaces
+                tail <- char '.' >> spaces >> parseExpr
+                return $ DottedList head tail
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+          <|> parseString
+          <|> parseNumber
+          <|> parseQuoted
+          <|> do char '('
+                 x <- (try parseList) <|> parseDottedList
+                 char ')'
+                 return x
+
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+instance Show LispVal where show = showVal
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote",val]) = val
+
+readExpr :: String -> LispVal
+readExpr input = case parse parseExpr "lisp" input of
+         Left err -> String $ "No match: " ++ show err
+         Right val -> val 
+
+main :: IO()
+main = getArgs >>= putStrLn . show . eval . readExpr . (!! 0)
+
